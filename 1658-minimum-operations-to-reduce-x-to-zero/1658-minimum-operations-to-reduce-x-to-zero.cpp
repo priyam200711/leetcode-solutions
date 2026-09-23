9c0f9670508f82cbef60b5a8cf9739bfd0c5b884
priyam200711 <kumarpriyam775406@gmail.com>
2026-09-23 17:41:38 +0530

    Time: 206 ms (12.32%), Space: 176.9 MB (5.19%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -3,30 +3,25 @@ public:
     int minOperations(vector<int>& nums, int x) {
         int n=nums.size();
         vector<int>suf(n);
-        vector<int>pref(n);
+        int pref=nums[n-1];
         suf[0]=nums[0];
         if(suf[0]==x)return 1;
-        pref[n-1]=nums[n-1];
-          if(pref[n-1]==x)return 1;
+       
+          if(pref==x)return 1;
         int ans=1e9;
         unordered_map<int,int>mp;
-        mp[pref[n-1]]=n-1;
+        mp[pref]=n-1;
         for(int i=1;i<n;i++){
             suf[i]=suf[i-1]+nums[i];
             if(suf[i]==x)ans=min(ans,i+1);
             }
         for(int i=n-2;i>=0;i--){
-            pref[i]=pref[i+1]+nums[i];
-            mp[pref[i]]=i;
-               if(pref[i]==x)ans=min(ans,n-i);
+            pref+=+nums[i];
+            mp[pref]=i;
+               if(pref==x)ans=min(ans,n-i);
             }
         
-        // for(int i=0;i<n;i++){
-        //     for(int j=n-1;j>i;j--){
-        //         if(pref[j]+suf[i] == x)ans=min(ans,i+n-j+1);
-        //           if(pref[j]+suf[i] > x)break;
-        //     }
-        // }
+     
         for(int i=0;i<n;i++){
             int rem=x-suf[i];
             if(rem<0)continue;
